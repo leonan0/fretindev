21be0490ea583e4e20bc9175d1e3e7e213ba62a3
Leonan <leonanf371@gmail.com>
2021-03-11 10:36:14 -0300

    site em contrusção

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,8 @@ import Image from 'next/image'
 import ViewSource from '../components/view-source'
 
 
-const Code = (p) => <code className={styles.inlineCode} {...p} />
-
-
 const Index = () => (
-    <div className={styles.container.bgText}>
-        Site em construção
-    <div className={styles.all}>
-        <div className={styles.g}>
-            <div className={styles.logo}>
-                <div className={styles.rec}></div>
-                <div className={styles.in}>in</div>
-                <div className={styles.fret}>fret</div>
-            </div>
-        </div>
-    </div>
-    </div>
+    <Image src='/Frame 1.png' width='1280' height='720%'></Image>
 )
 
 export default Index
